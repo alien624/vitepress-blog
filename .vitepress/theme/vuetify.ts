8b1b8f9d2e8598b319da5af40df0251f815c8a82
Alien <alien9@gmail.com>
2025-08-08 08:00:39 +0800

    chore: refactor vuetify plugin

---
+++ .vitepress/theme/vuetify.ts
@@ -0,0 +1,20 @@
+// .vitepress/theme/vuetify.ts
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+
+export default createVuetify({
+  ssr: true,
+  components,
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#1867C0",
+          secondary: "#5CBBF6",
+        },
+      },
+    },
+  },
+});
